8b407d46334c92f713591525e6790dd9e5098626
Ender Akay <eakay@wcc-group.com>
2019-03-22 15:32:42 +0100

    emoji support

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = async event => {
   const subject = event.queryStringParameters.name || 'World'
   return {
     statusCode: 200,
-    body: `Hi Zeynep I like your results on your last report, great job! &#129392 &#128406 &#128571`
+    body: `:sparkles: Hi Zeynep I like your results on your last report, great job! :bouquet: :+1: :blue_heart: :heart_eyes: </p>`
   }
 }
